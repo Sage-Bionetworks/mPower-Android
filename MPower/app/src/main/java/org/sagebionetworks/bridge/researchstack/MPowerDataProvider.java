ac710ceaa3bb9032cd7d54d80045a229ecbb50be
Eric Sieg <eric@sdpdigital.com>
2016-12-15 14:46:47 -0800

    Move project endpoint properties to gradle.properties

--- MPower/app/src/main/java/org/sagebionetworks/bridge/researchstack/MPowerDataProvider.java
+++ MPower/app/src/main/java/org/sagebionetworks/bridge/researchstack/MPowerDataProvider.java
@@ -5,21 +5,21 @@ import android.content.Context;
 import org.researchstack.backbone.ResourcePathManager;
 import org.researchstack.backbone.result.TaskResult;
 
+import static org.sagebase.mpower.BuildConfig.BRIDGE_URL;
+import static org.sagebase.mpower.BuildConfig.STUDY_ID;
+
 /**
  * Created by TheMDP on 12/12/16.
  */
 
 public class MPowerDataProvider extends BridgeDataProvider {
 
-    // TODO: move to gradle.properties??
-    static final String BASE_URL = "https://webservices.sagebridge.org/";
-    static final String STUDY_ID = "parkinson";
-
     static final String USER_AGENT = System.getProperty("http.agent");
 
     public MPowerDataProvider(Context context) {
         // TODO give path to permission file for uploads
-        super(BASE_URL, STUDY_ID, USER_AGENT,
+        // TODO add path to pem file
+        super(BRIDGE_URL, STUDY_ID, USER_AGENT,
                 new ResourcePathManager.Resource(ResourcePathManager.Resource.TYPE_HTML, "", ""));
     }
 
