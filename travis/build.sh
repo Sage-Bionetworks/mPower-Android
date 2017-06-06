6584e99fe427593e4f761908e3d26828fee469b3
Khai Do <zaro0508@gmail.com>
2017-06-06 14:54:06 -0700

    fix ci build
    * branch should match either master or release branch (stable-.*)
    * add command to list android virtual device targets before
      creating one.

--- travis/build.sh
+++ travis/build.sh
@@ -2,10 +2,10 @@
 set -ex
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then     # pull request
     bundle exec fastlane test
-elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "mPower-Android" ]]; then  # non-tag commits to master branch
+elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag commits to master branch
     bundle exec fastlane test
     bundle exec fastlane assemble
-elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^mPower-Android-.* ]]; then # non-tag commits to stable branches
+elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag commits to stable branches
     openssl aes-256-cbc -K $encrypted_d4d8881fb4c9_key -iv $encrypted_d4d8881fb4c9_iv -in travis.json.enc -out travis.json -d
     openssl aes-256-cbc -K $encrypted_d4d8881fb4c9_key -iv $encrypted_d4d8881fb4c9_iv -in sagebio.jks.enc -out sagebio.jks -d
     bundle exec fastlane alpha alias:"$ANDROID_KEY_ALIAS" storepass:"$ANDROID_KEYSTORE_PASSWORD" keypass:"$ANDROID_KEY_PASSWORD" signed_apk_path:"app/build/outputs/apk/app-release.apk"
