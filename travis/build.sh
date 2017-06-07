76f6e4cf00038af80b6160de10501f90a2d7008a
Khai Do <zaro0508@gmail.com>
2017-06-07 09:54:18 -0700

    stuff for testing travis builds

--- travis/build.sh
+++ travis/build.sh
@@ -6,8 +6,8 @@ elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag com
     bundle exec fastlane test
     bundle exec fastlane assemble
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag commits to stable branches
-    openssl aes-256-cbc -K $encrypted_d4d8881fb4c9_key -iv $encrypted_d4d8881fb4c9_iv -in travis.json.enc -out travis.json -d
-    openssl aes-256-cbc -K $encrypted_d4d8881fb4c9_key -iv $encrypted_d4d8881fb4c9_iv -in sagebio.jks.enc -out sagebio.jks -d
+    openssl aes-256-cbc -K $encrypted_d4d8881fb4c9_key -iv $encrypted_d4d8881fb4c9_iv -in $TRAVIS_BUILD_DIR/travis.json.enc -out $TRAVIS_BUILD_DIR/travis.json -d
+    openssl aes-256-cbc -K $encrypted_d4d8881fb4c9_key -iv $encrypted_d4d8881fb4c9_iv -in $TRAVIS_BUILD_DIR/sagebio.jks.enc -out $TRAVIS_BUILD_DIR/sagebio.jks -d
     bundle exec fastlane alpha alias:"$ANDROID_KEY_ALIAS" storepass:"$ANDROID_KEYSTORE_PASSWORD" keypass:"$ANDROID_KEY_PASSWORD" signed_apk_path:"app/build/outputs/apk/app-release.apk"
 fi
 exit $?
