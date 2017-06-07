6a3399af3ed6c02591a4ff6926eb8050d0ada5ce
Khai Do <zaro0508@gmail.com>
2017-06-07 11:05:18 -0700

    disable connected check due to issue MA-172

--- travis/build.sh
+++ travis/build.sh
@@ -8,6 +8,7 @@ elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag com
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag commits to stable branches
     openssl aes-256-cbc -K $encrypted_d4d8881fb4c9_key -iv $encrypted_d4d8881fb4c9_iv -in travis.json.enc -out travis.json -d
     openssl aes-256-cbc -K $encrypted_d4d8881fb4c9_key -iv $encrypted_d4d8881fb4c9_iv -in sagebio.jks.enc -out sagebio.jks -d
+    bundle exec fastlane test
     bundle exec fastlane alpha alias:"$ANDROID_KEY_ALIAS" storepass:"$ANDROID_KEYSTORE_PASSWORD" keypass:"$ANDROID_KEY_PASSWORD" signed_apk_path:"app/build/outputs/apk/app-release.apk"
 fi
 exit $?
